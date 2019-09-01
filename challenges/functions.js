830e3ca312b3d896d4d583dbd13dccbe5895ae1e
Brenda Joshua <brendajosh.bj@gmail.com>
2019-09-01 12:59:35 +0300

    completed functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,34 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb) {
+  return cb(a,b)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+//add
+function add(x, y) {
+  return x + y;
+}
 
+//multiply
+function multiply(x, y) {
+  return x * y;
+}
+
+//greeting
+function greeting(first_name, last_name){
+  return `Hello ${first_name} ${last_name}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,7 +42,7 @@
 
 // Explanation: 
 
-
+'nestedFunction can access the varible internal because closures can only look outside but cannot look within.'
 const external = "I'm outside the function";
 
 function myFunction() {
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,31 +7,31 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-Const tyrannosaurus = new Object({
+const tyrannosaurus = {
   objectName: "tyrannosaurus",
   diet: "carnivorous",
   weight: "7000kg",
   lenght: "12m",
   period: "Late Cretaceous",
-});
+}
 
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-const stegosaurus = new Object({
+const stegosaurus = {
   objectName: "stegosaurus",
   diet: "herbivorous",
   weight: "2000kg",
   lenght: "9m",
   period: "Late Jurassic",
-});
+};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-const velociraptor = new Object({
+const velociraptor = {
   objectName: "velociraptor",
   diet: "carnivorous",
   weight: "15kg",
   lenght: "1.8m",
   period: "Late Cretaceous",
-});
+};
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
@@ -86,7 +86,7 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = graduates.filter(graduate => graduate.university.includes("Uni"));
-console.log(uni);
+console.log(unisWithUni);
 
 // ==== ADVANCED Array Methods ====
 
@@ -111,7 +111,7 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
-zooAnimals.forEach((zooAnimal) => animalNames.push(zooAnimal.animal_name + '' +zooAnimal.scientific_name));
+zooAnimals.forEach((zooAnimal) => displayNames.push(zooAnimal.animal_name + '' +zooAnimal.scientific_name));
 console.log(displayNames);
 
 /* Request 2: .map()
